f43a095167f89350be9cfbbe4174152c3cb01b35
AmirAlakbarli <emirelekberli@gmail.com>
2022-10-17 17:10:23 +0400

    user, producr, basket and order models were created

---
+++ my-app/models/basket.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const basketSchema = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, "Please provide a userId!"],
+    },
+    products: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "product",
+        quantity: Number,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Basket = mongoose.model("basket", basketSchema);
+
+module.exports = Basket;
